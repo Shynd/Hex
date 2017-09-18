a22e261598b134e4f6b42d80bad6b3c40573c682
Shynd <afexlabs@gmail.com>
2017-09-19 00:57:33 +0200

    Added CreateFileW hook.

--- Hex-injected/dllmain.cpp
+++ Hex-injected/dllmain.cpp
@@ -6,13 +6,64 @@
 #pragma comment(lib, "detours.lib")
 #include <detours.h>
 
+/*
+ * CreateFileW(
+   _In_ LPCWSTR lpFileName,
+   _In_ DWORD dwDesiredAccess,
+   _In_ DWORD dwShareMode,
+   _In_opt_ LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+   _In_ DWORD dwCreationDisposition,
+   _In_ DWORD dwFlagsAndAttributes,
+   _In_opt_ HANDLE hTemplateFile
+);
+*/
+
+typedef HANDLE(WINAPI *CREATEFILEW)(
+	LPCWSTR					lpFileName,
+	DWORD					dwDesiredAccess,
+	DWORD					dwShareMode,
+	LPSECURITY_ATTRIBUTES	lpSecurityAttributes,
+	DWORD					dwCreationDisposition,
+	DWORD					dwFlagsAndAttributes,
+	HANDLE					h_template_file_handle
+	);
+CREATEFILEW OrigCreateFileW = NULL;
+
+HANDLE WINAPI HookCreateFileW(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode,
+							  LPSECURITY_ATTRIBUTES lpSecurityAttributes, DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes,
+							  HANDLE hTemplateFile) {
+	std::wcout << "CreateFileW: " << lpFileName << std::endl;
+
+	// Call the original function.
+	return OrigCreateFileW(lpFileName, dwDesiredAccess, dwShareMode, lpSecurityAttributes,
+						   dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
+}
+
+void InstallHooks() {
+	// Get Kernel32.
+	HMODULE hmodKernel32 = GetModuleHandle(TEXT("KERNEL32.dll"));
+
+	OrigCreateFileW = (CREATEFILEW)GetProcAddress(hmodKernel32, "CreateFileW");
+
+	// Install the hooks.
+	DetourTransactionBegin();
+	DetourUpdateThread(GetCurrentThread());
+
+	// CreateFileW hook
+	std::cout << "Attaching our CreateFileW hook.\n";
+	DetourAttach(&(PVOID&)OrigCreateFileW, HookCreateFileW);
+
+	DetourTransactionCommit();
+}
+
 BOOL APIENTRY DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
 	switch (ul_reason_for_call) {
 	case DLL_PROCESS_ATTACH:
 		AllocConsole();
 		freopen("CONOUT$", "w", stdout);
 		freopen("CONOUT$", "w", stderr);
-		//InstallHooks();
+		std::cout << "Installing hooks.\n";
+		InstallHooks();
 		break;
 	default:
 		break;
